fd60f4795db5c0368c5be1e6aa93d67bea26b90c
Hunter Davis <hunter@MisoBuntu1.(none)>
2012-03-20 11:22:41 -0700

    updated gitmodules to include impressjs added css styling added impressjs container updated javascript and html for impressjs

--- js/jsasl.js
+++ js/jsasl.js
@@ -1,25 +1,73 @@
-var displayASL = function(textToConvert, index, elementid, descriptionElement) {
-  var charToConvert = textToConvert.charAt(index);
+var insertImpressDiv = function(photoSrc, index, charToConvert,totalLength) {
+	// space out each slide by 2k 	
+	var yValue = index * 2000;
+
+	// build a div entry for each slide with basic impressJS
+	var divEntry = "<div id=\"asl";
+	divEntry += index + "\" ";
+	divEntry += "class=\"step slide\" data-x=\"0\" data-y=\"";
+	divEntry += yValue + "\" ";
+	divEntry += "data-scale=\"1\">";
+	divEntry += "<h1 align=\"center\">" + charToConvert + "</h1>";
+	divEntry += "<p class=\"centeredImage\"><image id=\"aslout\" src=\"";
+	divEntry += photoSrc + "\" ";
+	divEntry += "align=\"center\"></image></p>"
+	divEntry += "<div id=\"hint\">";
+	divEntry += "<h5 align=\"center\">";
+	divEntry += "Use a spacebar or arrow keys to navigate (";
+	divEntry += "Slide " + (index + 1) + "/" + totalLength + ")</h5></div></div>";
+
+	// insert the div into the document body under impress
+	document.getElementById("impress").innerHTML += divEntry;
+}
+
+var displayASL = function(textToConvert, index,totalLength) {
+  var charToConvert = textToConvert.charAt(index).toLowerCase();
   var badCharacters = "!@#$%^&*()+=-[]\\\';,./{}|\":<>?";
-  
-  document.getElementById(descriptionElement).innerHTML="<h1>" + charToConvert + "</h1>";
-  
+  var photoSrc = "";
+   
   if (badCharacters.indexOf(charToConvert) == -1) {
     if(charToConvert == " ")
     {
-      document.getElementById(elementid).src="../images/empty.png";   
+      photoSrc ="../images/empty.png";   
     } else {
-      document.getElementById(elementid).src="../images/" + charToConvert + ".png";
+      photoSrc ="../images/" + charToConvert + ".png";
     }
   } else {
-      document.getElementById(elementid).src="../images/empty.png";   
+      photoSrc ="../images/empty.png";   
   }
+
+  insertImpressDiv(photoSrc, index, charToConvert,totalLength);
 }
 
-var textToASL = function(textToConvert, index, elementid, descriptionElement) {
-  if(index < textToConvert.length) {
-    displayASL(textToConvert, index, elementid, descriptionElement);
-    index++;
-    setTimeout(function(){textToASL(textToConvert, index, elementid, descriptionElement)}, 1000);
-  }
+var textToASL = function(textToConvert) {
+	var totalLength = textToConvert.length;
+	for(var i = 0; i < totalLength; i++) {
+		displayASL(textToConvert, i,totalLength);
+	}
+
+	var impressLoad = document.createElement('script');
+	impressLoad.type = 'text/javascript'; 
+	impressLoad.async = true;
+	impressLoad.src = '../js/impressjs/js/impress.js';
+
+	impressLoad.onload = function () {
+	 impress();
+	}
+
+ 	var html_doc = document.getElementsByTagName('head')[0];
+	html_doc.appendChild(impressLoad);
+
+	// now remove our old input box
+	document.getElementById("submitbox").innerHTML = "";
 } 
+
+
+
+
+
+
+
+
+
+
